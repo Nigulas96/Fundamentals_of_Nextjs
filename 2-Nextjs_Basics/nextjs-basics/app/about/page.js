27d9ce8c4a0187a849159a62b64c1e092250ffb3
Nikolas Savolainen <ad2302@student.jamk.fi>
2026-08-10 19:12:41 +0300

    Next.js Basics - Task 2

---
+++ 2-Nextjs_Basics/nextjs-basics/app/about/page.js
@@ -0,0 +1,11 @@
+
+
+
+export default function AboutPage() {
+    return (
+        <>
+            <h1>About this course</h1>
+            <p>This course is about Next.js</p>
+        </>
+    );
+}
