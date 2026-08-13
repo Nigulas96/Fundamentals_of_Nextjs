acab4e82e25d14c3255aec888acee5b57590ce8a
Nikolas Savolainen <ad2302@student.jamk.fi>
2026-08-13 21:59:26 +0300

    Next.js Basics - Task 4

--- 2-Nextjs_Basics/nextjs-basics/app/page.tsx
+++ 2-Nextjs_Basics/nextjs-basics/app/page.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <h1>Hello, Next.js!</h1>
+      <h1>Hello Next.js App</h1>
+      <p>This application is built with Next.js</p>
       <Link href="/about">Go to About Page</Link>
     </>
   );
