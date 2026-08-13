e881e6e308ef441ef225290f274b788fb540bb83
Nikolas Savolainen <ad2302@student.jamk.fi>
2026-08-13 22:14:52 +0300

    Next.js Basics - Task 5

--- 2-Nextjs_Basics/nextjs-basics/app/page.tsx
+++ 2-Nextjs_Basics/nextjs-basics/app/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 import Link from 'next/link';
+import styles from './home.module.css';
 
 export default function Home() {
   return (
     <>
-      <h1>Hello Next.js App</h1>
+      <h1 className={styles.title}>Hello Next.js App</h1>
       <p>This application is built with Next.js</p>
       <Link href="/about">Go to About Page</Link>
     </>
