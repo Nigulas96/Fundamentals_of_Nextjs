77ce513cf278c20fb52fa9424fdf9b64fdc55970
Nikolas Savolainen <ad2302@student.jamk.fi>
2026-08-14 20:28:57 +0300

    Styling in Next.js - Task 2

--- 3-Styling_in_Nextjs/styling-app/app/page.tsx
+++ 3-Styling_in_Nextjs/styling-app/app/page.tsx
@@ -1,10 +1,12 @@
 import Title from "./components/Title";
-
+import Button from "./components/Button";
 export default function Home() {
 
   return (
+
     <main>
       <Title />
+      <Button />
     </main>
 
   );
