758feb1ff180865e599460f6fa6b89ce33db74cd
Nikolas Savolainen <ad2302@student.jamk.fi>
2026-08-14 19:50:53 +0300

    Styling in Next.js - Task 1

---
+++ 3-Styling_in_Nextjs/styling-app/app/components/Title.js
@@ -0,0 +1,11 @@
+import styles from "./Title.module.css";
+
+export default function Title() {
+
+    return (
+        <h1 className={styles.title}>
+            Title with style
+        </h1>
+    );
+
+}
