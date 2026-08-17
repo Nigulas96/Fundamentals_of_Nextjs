e0df7eb008f460dcf6dadd854428a60f7a619dfa
Nikolas Savolainen <ad2302@student.jamk.fi>
2026-08-17 18:01:30 +0300

    Styling in Nextjs - Task 5

--- 3-Styling_in_Nextjs/styling-app/app/page.tsx
+++ 3-Styling_in_Nextjs/styling-app/app/page.tsx
@@ -1,17 +1,21 @@
 import Title from "./components/Title";
 import Button from "./components/Button";
 import Card from "./components/Card";
+import AppLayout from "./components/AppLayout";
 
 export default function Home() {
 
   return (
 
     <main>
-      <Title />
-      <Button />
-      <Card highlighted={true} />
-      <Card highlighted={false} />
+      <AppLayout>
+        <Title />
+        <Button />
+        <Card highlighted={true} />
+        <Card highlighted={false} />
+      </AppLayout>
     </main>
+
 
   );
 
