4ea6675ce61c4dd0b09d38e7dd8589df484f3e9a
Nikolas Savolainen <ad2302@student.jamk.fi>
2026-08-21 13:01:35 +0300

    SSR and SSG - Task 2

--- 3-Styling_in_Nextjs/styling-app/app/blog/page.js
+++ 3-Styling_in_Nextjs/styling-app/app/blog/page.js
@@ -1,5 +1,12 @@
+export const revalidate = 10;
+
 export default async function Blog() {
-    const response = await fetch('https://api.vercel.app/blog');
+    const response = await fetch(
+        'https://api.vercel.app/blog',
+        {
+            next: { revalidate: 10 },
+        }
+    );
 
     const posts = await response.json();
 
@@ -7,10 +14,13 @@ export default async function Blog() {
         <>
             <h1>Blog</h1>
 
-            <ul>
+            <p>Last updated: {new Date().toLocaleString()}</p>
+
+            <ul style={{ listStyleType: 'disc', paddingLeft: '20px' }}>
                 {posts.map((post) => (
                     <li key={post.id}>{post.title}</li>
                 ))}
+
             </ul>
         </>
     );
