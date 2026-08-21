26a48ff5bb92517840bff59dbf17396cb8bf7881
Nikolas Savolainen <ad2302@student.jamk.fi>
2026-08-21 13:23:24 +0300

    SSR and SSG - Task 4

--- 3-Styling_in_Nextjs/styling-app/app/profile/page.js
+++ 3-Styling_in_Nextjs/styling-app/app/profile/page.js
@@ -1,4 +1,6 @@
 export default async function Profile() {
+    console.log('Rendering profile on server');
+
     const response = await fetch(
         'https://jsonplaceholder.typicode.com/users/1',
         {
