04b7503d438457b757d620bfd037d5f427167ce3
Nikolas Savolainen <ad2302@student.jamk.fi>
2026-08-22 09:53:28 +0300

    Testing - Task 3

--- 6-Testing/testing-app/tests/greeting.test.js
+++ 6-Testing/testing-app/tests/greeting.test.js
@@ -1,5 +1,7 @@
-import Greeting from '../utils/Greeting';
+import { render, screen } from '@testing-library/react';
+import Greeting from '../components/Greeting';
 
-test('returns greeting with name', () => {
-    expect(Greeting('Next.js')).toBe('Hello, Next.js!');
+test('renders greeting with name', () => {
+    render(<Greeting name="Next.js" />);
+    expect(screen.getByText('Hello, Next.js!')).toBeInTheDocument();
 });
